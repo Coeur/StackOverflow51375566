55998e6bfde3d8e19c70829d93631b55e07baa6c
Antoine Cœur <antoine.coeur@ef.com>
2018-09-03 17:28:13 +0800

    Sample project for EvilHydra code

---
+++ StackOverflow51375566/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  StackOverflow51375566
+//
+//  Created by Antoine Cœur on 2018/9/3.
+//  Copyright © 2018 coeur. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ StackOverflow51375566/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  StackOverflow51375566
+//
+//  Created by Antoine Cœur on 2018/9/3.
+//  Copyright © 2018 coeur. All rights reserved.
+//
+
+import UIKit
+
+func randomString(length: Int) -> String {
+    let charArray: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                                  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                                  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    let charArrayCount = UInt32(charArray.count)
+    var randomString = ""
+    for _ in 0 ..< length {
+        randomString += String(charArray[Int(arc4random_uniform(charArrayCount))])
+    }
+    return randomString
+}
+
+class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionLayout: UICollectionViewFlowLayout!
+    
+    var array: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionLayout.estimatedItemSize = CGSize(width: 50, height: 25)
+        collectionLayout.itemSize = UICollectionViewFlowLayout.automaticSize
+        collectionLayout.minimumInteritemSpacing = 10
+        
+        for _ in 0 ..< 1000 {
+            array.append(randomString(length: Int(arc4random_uniform(8))))
+        }
+        collectionView.reloadData()
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.label.text = array[indexPath.row]
+        return cell
+    }
+}
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
